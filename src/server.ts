c7927c68ba766dd8a7eea53e5d57c8d9aa0fa0c6
Iandraina Ravelomamana <iandraina.ravelomanana@edu.ece.fr>
2019-11-24 09:53:00 +0100

    part2

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express = require('express')
+import path = require('path')
+
+const app = express()
+const port: string = process.env.PORT || '8080'
+
+app.get('/', (req: any, res: any) => {
+  res.write('Hello world')
+  res.end()
+})
+
+app.listen(port, (err: Error) => {
+  if (err) {
+    throw err
+  }
+  console.log(`server is listening on port ${port}`)
+})
+
+
+import { MetricsHandler } from './metrics'
+app.get('/metrics.json', (req: any, res: any) => {
+  MetricsHandler.get((err: Error | null, result?: any) => {
+    if (err) {
+      throw err
+    }
+    res.json(result)
+  })
+})
